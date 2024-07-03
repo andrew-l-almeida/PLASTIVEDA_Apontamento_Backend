7cf893790cda3ae13f5c6a65771ccc30d8725f00
Andrew Almeida <andrew.l.almeida@outlook.com>
2024-07-03 11:51:30 -0300

    New Routes

--- databases/Areco.py
+++ databases/Areco.py
@@ -11,7 +11,6 @@ pd.options.display.max_rows = None
 class Areco:
     def __init__(self):
         self.metadata = MetaData()
-        
         self.create_engine(os.getenv('USER_ANDREW'), os.getenv('PASSWORD_ANDREW'))
         
         
@@ -138,14 +137,12 @@ order by 1,4
         params = (id_of, )
         operations = pd.read_sql_query(operations_stmt, self.engine, params=params)
         
-        print(operations)
-        
         records_stmt = """select A.id_of, 
 B.id_LstProcOF, 
 A.cd_of, 
 B.Seq_operacao, 
 C.id_operacao, 
-C.ds_operacao,
+rtrim(C.ds_operacao) as ds_operacao,
 isNull(E.QtdProduzida, 0) as QtdApontadaFabricacao,
 isNull(E.QtdRefugo, 0) as QtdApontadaRefugo,
 isNull(E.id_apontProd, 0) as id_apontProd,
@@ -154,12 +151,15 @@ format(E.Hora_Inicio, 'yyyy-MM-dd HH:mm:ss') as Hora_Inicio,
 format(E.DataFim, 'yyyy-MM-dd HH:mm:ss') as DataFim,
 format(E.HoraFim, 'yyyy-MM-dd HH:mm:ss') as HoraFim,
 IsNull(E.id_Prof_Intern, 0) as id_Prof_Intern,
-isNull(F.nome, '') as NomeApontador
+isNull(F.nome, '') as NomeApontador,
+isNull(G.id_maquina, 0) as id_Maquina,
+rtrim(G.ds_maquina) as ds_Maquina
 from OrdemFabricacao					A
 inner join LstProcOF					B on a.id_of = B.id_of
 inner join OperacoesProdutivas			C on C.id_operacao = B.id_operacao
 left join ApnProdTempoOnLine			E on E.id_lstProcOf = B.id_lstProcOf and E.id_of = B.id_of and E.pierSitReg = 'ATV'
 left join entidade						F on F.id_ent = E.id_prof_intern
+left join maquinas						G on G.id_maquina = E.id_maquina
 where A.pierSitReg = 'ATV'  and a.id_of = ?
 order by 1,4
         """
@@ -173,24 +173,29 @@ order by 1,4
             records_array = []
             
             for recordsIndex, records_row in records.iterrows():
+                record_dict = {}
                 if(records_row['id_LstProcOF'] == row['id_LstProcOF']):
-                        record_dict = {
-                            'id_apontProd': records_row['id_apontProd'],
-                            'Seq_operacao': records_row['Seq_operacao'],
-                            'ds_operacao': records_row['ds_operacao'],
-                            'QtdApontadaFabricacao': records_row['QtdApontadaFabricacao'],
-                            'QtdApontadaRefugo': records_row['QtdApontadaRefugo'],
-                            'dt_Inicio': records_row['dt_Inicio'],
-                            'Hora_Inicio': records_row['Hora_Inicio'],
-                            'DataFim': records_row['DataFim'],
-                            'HoraFim': records_row['HoraFim'],
-                            'id_Prof_Intern': records_row['id_Prof_Intern'],
-                            'NomeApontador': records_row['NomeApontador']
-                        }
-                        records_array.append(record_dict)
+                        if records_row['id_apontProd'] != 0:
+                            record_dict = {
+                                'id_apontProd': records_row['id_apontProd'],
+                                'Seq_operacao': records_row['Seq_operacao'],
+                                'ds_operacao': records_row['ds_operacao'],
+                                'QtdApontadaFabricacao': records_row['QtdApontadaFabricacao'],
+                                'QtdApontadaRefugo': records_row['QtdApontadaRefugo'],
+                                'dt_Inicio': records_row['dt_Inicio'],
+                                'Hora_Inicio': records_row['Hora_Inicio'],
+                                'DataFim': records_row['DataFim'],
+                                'HoraFim': records_row['HoraFim'],
+                                'id_Prof_Intern': records_row['id_Prof_Intern'],
+                                'NomeApontador': records_row['NomeApontador'],
+                                'id_maquina': records_row['id_Maquina'],
+                                'ds_maquina': records_row['ds_Maquina']
+                            }
+                            records_array.append(record_dict)
                         
             
             of_detail_dict = {
+                'id_of': row['id_of'],
                 'id_LstProcOf': row['id_LstProcOF'],
                 'Seq_operacao': row['Seq_operacao'],
                 'id_operacao': row['id_operacao'],
@@ -200,12 +205,152 @@ order by 1,4
             
             return_array.append(of_detail_dict)
         return return_array
+    
+    def get_operators(self):
         
+        stmt = """SELECT Entidade.id_ent as id_Ent  
+, Entidade.cdEnt AS cdEnt
+, rtrim(Entidade.Nome) AS Nome
+, Prof_Internos.id_Dept AS id_Dept
+, rtrim(Departamento.dsDept) AS dsDept
+, isNull(rtrim(CP.ds_celulaproducao), '') as CCusto
+, Prof_Internos.id_Prof_Intern
+ FROM Entidade AS Entidade  WITH(NOLOCK) 
+INNER JOIN RlcEntidadeTipo AS RlcEntidadeTipo  WITH(NOLOCK) ON (RlcEntidadeTipo.Id_Ent = Entidade.Id_Ent)
+INNER JOIN Tipo_Entid AS Tipo_Entid  WITH(NOLOCK) ON (Tipo_Entid.Id_Class = RlcEntidadeTipo.Id_Class)
+INNER JOIN Prof_Internos AS Prof_Internos  WITH(NOLOCK) ON (Prof_Internos.Id_Ent = Entidade.Id_Ent)
+INNER JOIN Departamento AS Departamento  WITH(NOLOCK) ON (Departamento.id_Dept = Prof_Internos.id_Dept)
+left join PlanoCusteio	as PC with(nolock) on (PC.id_CCusto = Prof_Internos.id_CCusto)
+left join CelulaProducao as CP with(nolock) on (cp.id_CCusto = PC.id_CCusto)
+WHERE (Entidade.PierSitReg = 'ATV')
+ AND (Tipo_Entid.cdClass = 4)
+ AND (Prof_Internos.id_Dept = 8)
+"""
+    
+        result = pd.read_sql_query(stmt, self.engine)
+        
+        setores = result['CCusto'].unique()
+        
+        return_array = []
+        
+        
+        for setor in setores:
+            department = {}
+            
+            department[setor] = []
+            
+            for index, row in result.iterrows():
+                
+                s = row['CCusto']
+                nome = row['Nome']
+                id_prof_intern = row['id_Prof_Intern']
+                id_ent = row['id_Ent'] 
+                
+                newPerson = {
+                    'nome': nome,
+                    'id_prof_intern': id_prof_intern,
+                    'id_ent': id_ent
+                }
+                
+                if setor == s:
+                    department[setor].append(newPerson)
+        
+            return_array.append(department)
+        
+        
+        
+        return return_array
+    
+    
+    def get_machines(self):
+        stmt = """SELECT Maquinas.id_maquina as id_maquina
+, rtrim(Maquinas.ds_maquina) AS ds_maquina
+, rtrim(CelulaProducao.ds_celulaproducao) AS CCusto
+, rtrim(CelulaProducao.Observacao) AS Observacao
+ FROM CelulaProducao AS CelulaProducao  WITH(NOLOCK) 
+INNER JOIN Entidade AS Entidade  WITH(NOLOCK) ON (Entidade.Id_Ent = CelulaProducao.id_Empresa)
+INNER JOIN PlanoCusteio AS PlanoCusteio  WITH(NOLOCK) ON (PlanoCusteio.id_CCusto = CelulaProducao.id_CCusto)
+LEFT JOIN MaquinasdaCelula AS MaquinasdaCelula  WITH(NOLOCK) ON (MaquinasdaCelula.id_celulaproducao = CelulaProducao.id_celulaproducao AND MaquinasdaCelula.PierSitReg = 'ATV')
+INNER JOIN Maquinas AS Maquinas  WITH(NOLOCK) ON (Maquinas.id_maquina = MaquinasdaCelula.id_maquina)
+WHERE (Entidade.cdEnt = 1)
+ AND (CelulaProducao.PierSitReg = 'ATV')
+
+ORDER BY CelulaProducao.cd_celulaproducao
+        """
+        
+        result = pd.read_sql_query(stmt, self.engine)
+        
+        setores = result['CCusto'].unique()
+        
+        return_array = []
+        
+        for setor in setores:
+            department = {}
+            
+            department[setor] = []
+            
+            for index, row in result.iterrows():
+                
+                s = row['CCusto']
+                ds_maquina = row['ds_maquina']
+                id_maquina = row['id_maquina']
+                observacao = row['Observacao']
+                
+                newMachine = {
+                    'ds_maquina': ds_maquina,
+                    'id_maquina': id_maquina,
+                    'observacao': observacao
+                }
+                
+                if setor == s:
+                    department[setor].append(newMachine)
+        
+            return_array.append(department)
+        return return_array
+    
+    def insert_new_record(self, data):
+        id_of = data['id_of'] 
+        id_LstProcOF = data['id_LstProcOF']
+        id_operacao = data['id_operacao']
+        id_maquina = data['id_maquina']
+        id_Prof_Intern = data['id_Prof_Intern']
+        dateTime = data['dateTime'] 
+        hour = data['hour']
+        
+        stmt = """exec ProcInsUpdApnProdTempoOnLine 0, :id_of, :id_LstProcOF, 1, :id_operacao, :id_maquina, :id_Prof_Intern, 0, :dateTime, :hour, NULL,'1900-01-02 00:00:00', 0, 0, 'N','ATV'"""
+        
+        params = dict(id_of = id_of, id_LstProcOF = id_LstProcOF, id_operacao = id_operacao, id_maquina = id_maquina, id_Prof_Intern= id_Prof_Intern, dateTime = dateTime, hour = hour)
+        
+        with self.engine.connect() as conn:
+            result = conn.execute(text(stmt), params).scalar()
+            conn.commit()
+            return result
+        
+    def finalize_record(self, data):
+        DataFim = data['DataFim']
+        HoraFim = data['HoraFim']
+        QtdProduzida = data['QtdProduzida']
+        QtdRefugo = data['QtdRefugo']
+        id_apontProd = data['id_apontProd']
+        
+        
+        stmt = """ update ApnProdTempoOnline
+                set DataFim = :DataFim, HoraFim = :HoraFim, QtdProduzida = :QtdProduzida, QtdRefugo = :QtdRefugo
+                where id_apontProd = :id_apontProd
+                """       
+                
+        params = dict(DataFim = DataFim, HoraFim = HoraFim, QtdProduzida = QtdProduzida, QtdRefugo = QtdRefugo, id_apontProd = id_apontProd)
+        
+        with self.engine.connect() as conn:
+            result = conn.execute(text(stmt), params)
+            conn.commit()
+            return id_apontProd
         
     def create_engine(self, uid, pwd):
         connection_string = f"DRIVER={{{os.getenv('DRIVER')}}};SERVER={os.getenv('SERVER')};DATABASE={os.getenv('DATABASE')};UID={uid};PWD={pwd}"
         url = URL.create('mssql+pyodbc', query={'odbc_connect': connection_string})
         self.engine = create_engine(url)
+        print(os.getenv('SERVER'), os.getenv('DATABASE'))
         
         
 areco = Areco()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import databases
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -29,17 +29,48 @@ def getOrders():
         orders = databases.areco.get_orders()
         return jsonify({'orders': orders})
     
-@app.route('/getOrderDetails/<int:id_ped>')
+@app.route('/getOrderDetails/<int:id_ped>', methods=['GET'])
 @limiter.exempt
 def getOrderDetails(id_ped):
     order_details = databases.areco.get_order_details(id_ped)
     return jsonify({'order': order_details})
 
-@app.route('/getOfDetails/<int:id_of>')
+@app.route('/getOfDetails/<int:id_of>', methods=['GET'])
 @limiter.exempt
 def getOfDetails(id_of):
     of_details = databases.areco.get_of_details(id_of)
     return jsonify({'of_details': of_details})
+
+
+@app.route('/getOperators', methods=['GET'])
+@limiter.exempt
+def getOperators():
+    setores = databases.areco.get_operators()
+    return jsonify({'setores': setores})
+
+@app.route('/getMachines', methods=['GET'])
+@limiter.exempt
+def getMachines():
+    machines = databases.areco.get_machines()
+    return jsonify({'machines': machines})
+
+@app.route('/insertNewRecord', methods=['POST'])
+@limiter.exempt
+def insertNewRecord():
+    data = request.json
+    
+    new_record = databases.areco.insert_new_record(data)
+    print(new_record)
+    return jsonify({'id_apontProd': new_record})
+
+@app.route('/finalizeRecord', methods=['POST'])
+@limiter.exempt
+def finalizeRecord():
+    data = request.json
+    
+    finalizedRecord = databases.areco.finalize_record(data)
+    
+    return jsonify({'id_apontProd': finalizedRecord})
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
